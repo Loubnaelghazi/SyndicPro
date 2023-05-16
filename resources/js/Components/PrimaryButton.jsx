a071bc1159bf8a0d6c95729823c2d5d369e6e705
Loubnaelghazi <loubna.elghazi1@etu.uae.ac.ma>
2023-05-17 01:58:21 +0200

    tailwind config

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,10 +1,15 @@
-export default function PrimaryButton({ className = '', disabled, children, ...props }) {
+export default function PrimaryButton({
+    className = "",
+    disabled,
+    children,
+    ...props
+}) {
     return (
         <button
             {...props}
             className={
-                `flex w-full justify-center rounded-md bg-primary-color px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-color ${
-                    disabled && 'opacity-25'
+                `flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-color ${
+                    disabled && "opacity-25"
                 } ` + className
             }
             disabled={disabled}
--- resources/js/Layouts/GuestLayout.jsx
+++ resources/js/Layouts/GuestLayout.jsx
@@ -3,7 +3,7 @@ import { Link } from '@inertiajs/react';
 
 export default function Guest({ children }) {
     return (
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 bg- py-12 lg:px-8">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 bg-white py-12 lg:px-8">
             <div className="w-6/12 mx-auto">
                 <div className="p-12  shadow-csh bg-white rounded-xl  overflow-hidden">
                     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
