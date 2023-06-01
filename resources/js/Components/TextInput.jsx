3e563ec37a0802c01742a30ba97cd2e5d0632d4c
med-el-mrabet <momt20003@gmail.com>
2023-06-01 04:34:45 +0100

    add and modify depense edit

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,6 +1,6 @@
 import { forwardRef, useEffect, useRef } from 'react';
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+export default forwardRef(function TextInput({ type = 'text', className = 'mb-4', isFocused = false, ...props }, ref) {
     const input = ref ? ref : useRef();
 
     useEffect(() => {
@@ -15,7 +15,7 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
             {...props}
             type={type}
             className={
-                `block w-full rounded-md mb-4 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-color sm:text-sm sm:leading-6` +
+                `block w-full rounded-md  border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-color sm:text-sm sm:leading-6 ` +
                 className
             }
             ref={input}
--- resources/js/Pages/depenses/ModifierDepense.jsx
+++ resources/js/Pages/depenses/ModifierDepense.jsx
@@ -1,159 +1,157 @@
-import React from 'react'
+import React from "react";
 import { Head } from "@inertiajs/react";
 import Main_content from "@/main _content/Main_content";
+import TextInput from "@/Components/TextInput";
+import PrimaryButton from "@/Components/PrimaryButton";
 
-export default function ModifierDepense({auth}) {
-  return (
-      <Main_content
-          user={auth.user}
-          Title={"Modifier dépense"}
-          Description={"test test test "}
-          ClassName="p-0"
-      >
-          <Head title=" Modifier dépenses" />
-          <form className="p-6 pt-8">
-              <div className=" bg-white rounded-20  shadow-md p-6">
-                  <div className="mb-4">
-                      <div>
-                          <div className="flex flex-col gap-5">
-                              <div className="">
-                                  <label
-                                      htmlFor="fournissuer"
-                                      className="font-medium"
-                                  >
-                                      Fourisseur :
-                                  </label>
-                                  <div className="flex flex-row items-center gap-4 mt-2">
-                                      <select
-                                          name="fournisseur"
-                                          id="fournisseur"
-                                          className="w-full h-10 px-2 border border-gray-300 rounded  focus:ring-primary-color focus:border-purple-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                                      >
-                                          <option
-                                              disabled
-                                              value="Le fournisseur"
-                                          >
-                                            Le fournisseur
-                                          </option>
-                                          <option value=" ">1</option>
-                                          <option value="">2</option>
-                                      </select>
-                                      <div className="flex flex-row text-gray-400">
-                                          {" "}
-                                          <div>-</div> <div>ou</div>
-                                          <div>-</div>
-                                      </div>
-                                      <input
-                                          type="text"
-                                          class="w-full h-10 px-2 border border-gray-300 rounded focus:ring-purple-500 focus:border-purple-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                                      ></input>
-                                  </div>
-                              </div>
-                              <div>
-                                  <label
-                                      htmlFor="designation"
-                                      className="font-medium"
-                                  >
-                                      Désignation :
-                                  </label>
-                                  <select
-                                      name="designation"
-                                      id="designation"
-                                      className="w-full mt-2 h-10 px-2 border border-gray-300 rounded  focus:ring-primary-color focus:border-purple-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                                  >
-                                      <option
-                                          
-                                          value="Décoration de l'immeuble "
-                                      >
-                                          Décoration de l'immeuble
-                                      </option>
-                                      <option value=" "></option>
-                                      <option value=""></option>
-                                  </select>
-                              </div>
-                              <div>
-                                  <label htmlFor="date" className="font-medium">
-                                      Date de paiement :
-                                  </label>
-                                  <input
-                                      type="date"
-                                      name="date"
-                                      id="date"
-                                      className="w-full mt-2 h-10 px-2 border border-gray-300 rounded  focus:ring-primary-color focus:border-purple-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                                      placeholder="Date de la réunion"
-                                      required
-                                  />
-                              </div>
-                              <div>
-                                  <label
-                                      htmlFor="montant"
-                                      className="font-medium"
-                                  >
-                                      {" "}
-                                      Montant :
-                                  </label>
-                                  <input
-                                      type="text"
-                                      name="montant"
-                                      id="montant"
-                                      className="w-full mt-2 h-10 px-2 border border-gray-300 rounded  focus:ring-primary-color focus:border-purple-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                                      placeholder="Montant (DH)"
-                                  />
-                              </div>
-                              <div>
-                                  <label
-                                      htmlFor="statut"
-                                      className="font-medium"
-                                  >
-                                      {" "}
-                                      Statut :{" "}
-                                  </label>
-                                  <select
-                                      name="statut"
-                                      id="statut"
-                                      className="w-full mt-2 h-10 px-2 border border-gray-300 rounded  focus:ring-primary-color focus:border-purple-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-purple-500 dark:focus:border-purple-500"
-                                  >
-                                      <option disabled value="Statut">
-                                          {" "}
-                                          Statut :
-                                      </option>
-                                      <option value="Payée">Payée</option>
-                                      <option value="Partiellement payée ">
-                                          Partiellement payée
-                                      </option>
-                                      <option value=" Non payée">
-                                          {" "}
-                                          Non payée
-                                      </option>
-                                  </select>
-                              </div>
-                              <div>
-                                  <label
-                                      htmlFor="description "
-                                      className="font-medium"
-                                  >
-                                      Description :
-                                  </label>
-                                  <textarea
-                                      id="description"
-                                      placeholder="Vous pouvez modifier vos informations içi "
-                                      rows="2"
-                                      className="block mt-2  p-2.5 w-full text-l text-gray-900  rounded-lg border border-gray-300 focus:ring-primary-color focus:border-primary-color dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-color dark:focus:border-primary-color h-min"
-                                  ></textarea>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              </div>
-              <div className="mt-4 flex justify-center ">
-                  <button
-                      type="submit"
-                      className="px-4 py-2 w-40 bg-primary-color  hover:opacity-90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-color  text-white rounded-md mr-2"
-                  >
-                      Enregistrer
-                  </button>
-              </div>
-          </form>
-      </Main_content>
-  );
+
+export default function ModifierDepense({ auth }) {
+    return (
+        <Main_content
+            user={auth.user}
+            Title={"Modifier dépense"}
+            Description={"test test test "}
+            ClassName="p-12"
+        >
+            <Head title=" Modifier dépenses" />
+            <form className="">
+                <div className=" ">
+                    <div className="">
+                        <div>
+                            <div className="flex flex-col gap-5">
+                                <div>
+                                    <label
+                                        htmlFor="designation"
+                                        className="font-medium"
+                                    >
+                                        Désignation :
+                                    </label>
+                                    <TextInput
+                                        name="designation"
+                                        type="text"
+                                        id="designation"
+                                        className="mb-0"
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="description "
+                                        className="font-medium"
+                                    >
+                                        Description :
+                                    </label>
+                                    <textarea
+                                        id="description"
+                                        placeholder="Vous pouvez ajouter plus d'informations içi "
+                                        rows="3"
+                                        className="block mt-2  p-2.5 w-full text-l text-gray-900  rounded-lg border border-gray-300 focus:ring-primary-color focus:border-primary-color dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-color dark:focus:border-primary-color h-min"
+                                    ></textarea>
+                                </div>
+
+                                <div className="">
+                                    <label
+                                        htmlFor="fournissuer"
+                                        className="font-medium"
+                                    >
+                                        Fourisseur :
+                                    </label>
+                                    <div className="flex flex-row items-center gap-4">
+                                        <select
+                                            name="fournisseur"
+                                            id="fournisseur"
+                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-color sm:text-sm sm:leading-6"
+                                        >
+                                            <option
+                                                disabled
+                                                value="Selectionez le fournisseur"
+                                            >
+                                                Selectionez le fournisseur
+                                            </option>
+                                            <option value=" ">1</option>
+                                            <option value="">2</option>
+                                        </select>
+                                        <div className="flex flex-row text-gray-400">
+                                            {" "}
+                                            <div>-</div> <div>ou</div>
+                                            <div>-</div>
+                                        </div>
+                                        <TextInput
+                                            type="text"
+                                            placeholder="Entrer un nom"
+                                            className="mb-0"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="montant"
+                                        className="font-medium"
+                                    >
+                                        {" "}
+                                        Montant :
+                                    </label>
+                                    <TextInput
+                                        type="text"
+                                        name="montant"
+                                        id="montant"
+                                        placeholder="Montant (DH)"
+                                        className="mb-0"
+                                    />
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="statut"
+                                        className="font-medium"
+                                    >
+                                        {" "}
+                                        Statut :{" "}
+                                    </label>
+                                    <select
+                                        name="statut"
+                                        id="statut"
+                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-color sm:text-sm sm:leading-6"
+                                    >
+                                        <option disabled value="Statut">
+                                            {" "}
+                                            Statut :
+                                        </option>
+                                        <option value="Payée">Payée</option>
+                                        <option value="Partiellement payée ">
+                                            Partiellement payée
+                                        </option>
+                                        <option value=" Non payée">
+                                            {" "}
+                                            Non payée
+                                        </option>
+                                    </select>
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor="date"
+                                        className="font-medium"
+                                    >
+                                        Date de paiement :
+                                    </label>
+                                    <input
+                                        type="date"
+                                        name="date"
+                                        id="date"
+                                        className="w-full mt-2 h-10 px-2 border border-gray-300 rounded  focus:ring-primary-color focus:border-purple-500 block  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white  dark:focus:ring-purple-500 dark:focus:border-purple-500"
+                                        placeholder="Date de la réunion"
+                                        required
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-4 flex justify-center ">
+                    <PrimaryButton type="submit" className="w-40  py-2 ">
+                        Ajouter
+                    </PrimaryButton>
+                </div>
+            </form>
+        </Main_content>
+    );
 }
